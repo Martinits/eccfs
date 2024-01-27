f0890d1332108281bf73aff963ffa16d63f37132
Martinits <mamartinitramam@163.com>
2024-01-27 12:21:32 +0800

    rw: empty test

--- src/rw/builder.rs
+++ src/rw/builder.rs
@@ -536,6 +536,76 @@ impl RWBuilder {
 #[cfg(test)]
 mod test {
     #[test]
+    fn build_empty() {
+        use std::path::Path;
+        use crate::*;
+        use std::fs::OpenOptions;
+        use std::fs;
+        use std::env;
+        use std::io::prelude::*;
+        #[allow(unused)]
+        use rand_core::RngCore;
+
+        if cfg!(debug_assertions) {
+            env::set_var("RUST_BACKTRACE", "1");
+            env_logger::builder()
+                .filter_level(log::LevelFilter::Debug)
+                .init();
+        }
+
+        let args: Vec<String> = env::args().collect();
+        assert!(args.len() >= 5);
+        let mode = args[3].clone();
+        let target = args[4].clone();
+        debug!("Creating empty RWFS {}", target);
+
+        let to = format!("test/{}.rwimage", &target);
+
+        let k = match mode.as_str() {
+            "enc" => {
+                let mut k = [0u8; 16];
+                rand::thread_rng().fill_bytes(&mut k);
+                Some(k)
+            }
+            "int" => {
+                None
+            }
+            _ => panic!("unrecognized fsmode"),
+        };
+
+        let mode = super::create_empty(
+            Path::new(&to),
+            k,
+        ).unwrap();
+        match &mode {
+            FSMode::IntegrityOnly(hash) => {
+                let s = hex::encode_upper(hash);
+                println!("Built in IntegrityOnly Mode:");
+                println!("Hash: {}", s);
+            }
+            FSMode::Encrypted(key, mac) => {
+                assert_eq!(k.unwrap(), *key);
+                println!("Built in Encrypted Mode:");
+                let k = hex::encode_upper(key);
+                let m = hex::encode_upper(mac);
+                println!("Key: {}", k);
+                println!("Mac: {}", m);
+            }
+        }
+        // save mode to file
+        let name = format!("test/{}.mode", target);
+        let _ = fs::remove_file(name.clone());
+        let mut f = OpenOptions::new().write(true).create_new(true).open(name).unwrap();
+        let written = f.write(unsafe {
+            std::slice::from_raw_parts(
+                &mode as *const FSMode as *const u8,
+                std::mem::size_of::<FSMode>(),
+            )
+        }).unwrap();
+        assert_eq!(written, std::mem::size_of::<FSMode>());
+    }
+
+    #[test]
     fn build_rw() {
         use std::path::Path;
         use crate::*;
